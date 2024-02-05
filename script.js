17c8b3f3e7174d3cbe45c801926fa04da991070f
Lucas Quadros <olucasquadros@gmail.com>
2024-02-05 20:30:35 -0300

    Mensagem descritiva do commit

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ function calcularMedia() {
     let nota2 = parseFloat(document.getElementById('nota2').value);
     let nota3 = parseFloat(document.getElementById('nota3').value);
 
-    let media = (nota1 + nota2 + nota3) / 3;
+    let media =  (nota1 + nota2 + nota3) /3;
 
-    document.getElementById('resultado').innerHTML = `Média: ` + media;
-    var situacao = document.querySelector(`#situacao`);
-    
+    document.getElementById('resultado').innerHTML = `A média é: ` + media.toFixed(2);
 
-    
-
+    if(media>7) {
+        document.getElementById('situacao').innerHTML = `Está Aprovado`;
+        document.getElementById('situacao').style.color = 'green';
+    } else if(media === 6) {
+        document.getElementById('situacao').innerHTML = `Em Recuperação`;
+        document.getElementById('situacao').style.color = 'orange';
+    } 
+    else{
+        document.getElementById('situacao').innerHTML = `Está Reprovado`;
+        document.getElementById('situacao').style.color = 'red';
+        document.getElementById('situacao').style.backgroundColor = 'black';
+    }
 }
